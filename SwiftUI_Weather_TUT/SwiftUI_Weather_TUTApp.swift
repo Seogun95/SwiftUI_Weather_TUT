854893efcbd3151e0f5f4ab7ea99303e301e0bcc
SeonJungKim <ksj083895@gmail.com>
2021-03-06 09:30:34 +0500

    Initial Commit

---
+++ SwiftUI_Weather_TUT/SwiftUI_Weather_TUTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Weather_TUTApp.swift
+//  SwiftUI_Weather_TUT
+//
+//  Created by 김선중 on 2021/03/06.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Weather_TUTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
